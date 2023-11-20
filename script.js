65c9e7dfc7db57e1b1d429cd75b425fe345c5ba8
Anna Zubkova <annakemik@gmail.com>
2023-11-20 12:20:10 -0500

    Added lesson 5

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let num = 266219;
+const arr = ["3584938", "8292", "492857", "4833", "920", "232", "58292"];
 
-const stringFromNumber = num.toString();
-
-let result = 1;
-
-for (let i = 0; i < stringFromNumber.length; i++) {
-  result *= stringFromNumber[i];
+for (let i = 0; i < arr.length; i++) {
+  let num = Number(arr[i].charAt(0));
+  if (num == 2 || num == 4) console.log(arr[i]);
 }
 
-result **= 3;
+const isSimple = function (number) {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
-let newString = result.toString();
-
-result = console.log(newString[0] + newString[1]);
+for (let i = 1; i < 101; i++) {
+  if (isSimple(i)) {
+    console.log(i, "Делители этого числа: 1 и " + i);
+  }
+}
